aa0bd8eacdf3bb048e3d6d26ac62f20fffe3e252
Mike <m.chuachong@gmail.com>
2025-01-24 08:27:03 -0500

    commit

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
+import { Octokit } from "octokit";
+
 console.log("test3")
 
 const text = document.getElementById("text");
 const btn = document.getElementById("new-quote");
-const quotesUrl = "https://zenquotes.io/api/random"
+const quotesUrl = 
+"'https://famous-quotes4.p.rapidapi.com/random?category=all&count=2'"
 
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'd73081fa78msh47109fdeafb6015p111c25jsn6510ec03d95a',
+		'x-rapidapi-host': 'famous-quotes4.p.rapidapi.com'
+	}
+}
 
-const getQuote = () => {
+console.log("test")
+
+const getQuote = async () => {
     text.textContent="Clicked!"
-    const res = fetch (quotesUrl);
-    const data = res.JSON();
-    console.log(data);
+    console.log("clicked")
+    const res = await fetch (quotesUrl,options);
+    const data = await res.json();
+    text.textContent=data;
     
 }
 
